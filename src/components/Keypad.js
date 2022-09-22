acfe1f7953bafef129894f1cd315a4ecda8277af
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-22 10:15:37 +0300

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function HandleKeypress() {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input onChange={HandleKeypress} type='password'></input>
+        </div>
     )
 }
 
